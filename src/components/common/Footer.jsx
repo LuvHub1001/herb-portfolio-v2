12333a9964fb208b0a18f9d6fa08c6f8b162d1c5
dy7791 <dy7791@naver.com>
2025-04-28 15:20:25 +0900

    fix:: Footer style

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="flex min-w-screen h-25 items-center justify-center border-1">
+    <div className="flex min-w-screen h-25 mt-10 border-t-3 border-[#FAFAFA] items-center justify-center ">
       ⓒ 2025. Herb Corp. All rights reserved.
     </div>
   );
