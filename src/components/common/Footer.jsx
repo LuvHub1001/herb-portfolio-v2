ac2db6c78ec2cd49921c01828153138368906702
dy7791 <dy7791@naver.com>
2025-04-28 11:28:22 +0900

    add:: Footer

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="flex min-w-screen h-25 items-center justify-center border-1">
+      ⓒ 2025. Herb Corp. All rights reserved.
+    </div>
+  );
+}
+
+export default Footer;
