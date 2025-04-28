1c9dba4ea7f25e760f776d3e39c2bf14d69967f6
dy7791 <dy7791@naver.com>
2025-04-28 10:40:45 +0900

    App 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Suspense } from "react";
+import { Loading, ErrorBoundary } from "./components";
+
 import "./App.css";
 
 function App() {
-  return <div>TEST</div>;
+  return (
+    <ErrorBoundary>
+      <Suspense fallback={<Loading />}></Suspense>
+    </ErrorBoundary>
+  );
 }
 
 export default App;
