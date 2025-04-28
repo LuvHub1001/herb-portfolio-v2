a9e883220c9e416587721171b39733ed21c73d24
dy7791 <dy7791@naver.com>
2025-04-28 11:28:29 +0900

    add:: Sidebar

---
+++ src/components/common/Sidebar.jsx
@@ -0,0 +1,16 @@
+function Sidebar() {
+  return (
+    <div>
+      <div className="w-[280px] h-screen border-1 border-red-400">
+        <div className="flex flex-col">
+          <div>이동엽</div>
+          <div>
+            <img src="/images/취업사진.jpg" alt="이미지" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
