57ec5ed13ff6fd1d4207d4b280c703435cf97194
dy7791 <dy7791@naver.com>
2025-04-28 11:28:39 +0900

    code split

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import ErrorBoundary from "./common/ErrorBoundary";
 import Loading from "./common/Loading";
+import Sidebar from "./common/Sidebar";
+import Footer from "./common/Footer";
 
-export { ErrorBoundary, Loading };
+export { ErrorBoundary, Loading, Sidebar, Footer };
