376a2096b8a097cd99043cc26eb0c413cd116923
dy7791 <dy7791@naver.com>
2025-04-28 15:20:34 +0900

    fix:: hoc

--- src/hoc/withCommonLayout.jsx
+++ src/hoc/withCommonLayout.jsx
@@ -3,11 +3,12 @@ import { Sidebar, Footer } from "../components";
 function withCommonLayout(Component) {
   const WrappedComponent = () => {
     return (
-      <div>
+      <div className="overflow-x-hidden">
         <Sidebar />
         <div className="ml-[280px]">
           <Component />
         </div>
+        <Footer />
       </div>
     );
   };
