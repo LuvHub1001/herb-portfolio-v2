51c689a46e6ac10998feec774cd73d55c4916fec
dy7791 <dy7791@naver.com>
2025-04-29 15:21:52 +0900

    라우팅 추가

--- src/routes/PublicRouter.jsx
+++ src/routes/PublicRouter.jsx
@@ -1,6 +1,7 @@
 import { lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { withCommonLayout } from "../hoc";
+import { ScrollToTop } from "../components";
 
 const MainPage = lazy(() => import("../pages/MainPage"));
 const WrappedMainPage = withCommonLayout(MainPage);
@@ -8,12 +9,20 @@ const WrappedMainPage = withCommonLayout(MainPage);
 const PortfolioPage = lazy(() => import("../pages/PortfolioPage"));
 const WrappedPortfolioPage = withCommonLayout(PortfolioPage);
 
+const PortfolioDetailPage = lazy(() => import("../pages/PortfolioDetailPage"));
+const WrappedPortfolioDetailPage = withCommonLayout(PortfolioDetailPage);
+
 function PublicRouter() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<WrappedMainPage />} />
         <Route path="/portfolio" element={<WrappedPortfolioPage />} />
+        <Route
+          path="/portfolio/:title"
+          element={<WrappedPortfolioDetailPage />}
+        />
       </Routes>
     </BrowserRouter>
   );
